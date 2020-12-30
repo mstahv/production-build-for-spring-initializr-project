290ba653e6079793852fd5fdaa688028a50da445
Matti Tahvonen <matti@vaadin.com>
2020-12-30 12:35:09 +0200

    View class for testing

---
+++ src/main/java/com/example/demo/MainView.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route
+public class MainView extends VerticalLayout {
+    public MainView() {
+        add(new Paragraph("Hello Spring Boot"));
+    }
+}
